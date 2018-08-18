93f6f07cf7a69cdcb244f488365ea4b39059af9a
XinMei Lin <xinmeilin04@gmail.com>
2018-08-18 21:56:54 +0000

    links

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,8 +3,16 @@ class TodoController < ApplicationController
   end
   
   def show
-      @todo_description="Make the cirriculum"
-      @todo_pomodoro_estimate=4
+    if params[:id]=="1" 
+        @todo_description="Make the cirriculum"
+        @todo_pomodoro_estimate=4
+    elsif params[:id]=="2"
+        @todo_description="Make Food"
+        @todo_pomodoro_estimate=3
+    elsif params[:id]=="3"
+        @todo_description="Wake Up on Time"
+        @todo_pomodoro_estimate=3
+    end
+    
   end
-  
 end
