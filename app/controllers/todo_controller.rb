921f1093d484dde0ebeebe85ac20f58ab8673703
XinMei Lin <xinmeilin04@gmail.com>
2018-08-19 18:38:37 +0000

    new buttons

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,5 +5,18 @@ class TodoController < ApplicationController
   def show
     @t=Todo.find(params[:id])
   end
-    
+  
+  def new
+     
+  end
+  
+  def create
+     t=Todo.new 
+     t.description=params[:description]
+     t.pomodoros_estimate=params[:pomodoros_estimate]
+     t.completed=false
+     t.save
+     redirect_to "/todo/show/#{t.id}"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "todos", to:"todo#index"
   get "todo/show/:id", to: "todo#show"
+  get "todo/new", to:"todo#new"
+  get "todo/create", to: "todo#create"
 end
