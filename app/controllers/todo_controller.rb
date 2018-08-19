eaa21a44e0b56fd675203865f07bc6eb179610f4
XinMei Lin <xinmeilin04@gmail.com>
2018-08-19 17:27:29 +0000

    New DB List

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,16 +3,7 @@ class TodoController < ApplicationController
   end
   
   def show
-    if params[:id]=="1" 
-        @todo_description="Make the cirriculum"
-        @todo_pomodoro_estimate=4
-    elsif params[:id]=="2"
-        @todo_description="Make Food"
-        @todo_pomodoro_estimate=3
-    elsif params[:id]=="3"
-        @todo_description="Wake Up on Time"
-        @todo_pomodoro_estimate=3
-    end
-    
+    @t=Todo.find(params[:id])
   end
+    
 end
